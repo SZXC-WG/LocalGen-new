6b71c6cee15469af0e22fa64802fc0d99fd8649a
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2023-01-06 16:00:50 +0800

    update url link

--- LGcons.hpp
+++ LGcons.hpp
@@ -1,7 +1,7 @@
 /* This is LGcons.hpp file of LocalGen.                                  */
 /* Copyright (c) 2023 LocalGen-dev; All rights reserved.                 */
 /* Developers: http://github.com/LocalGen-dev                            */
-/* Project: http://github.com/LocalGen-dev/Local-Generals.io             */
+/* Project: http://github.com/LocalGen-dev/LocalGen-new                  */
 /*                                                                       */
 /* This project is licensed under the MIT license. That means you can    */
 /* download, use and share a copy of the product of this project. You    */
@@ -9,7 +9,7 @@
 /* must print the copyright information at the front of your product.    */
 /*                                                                       */
 /* The full MIT license this project uses can be found here:             */
-/* http://github.com/LocalGen-dev/LocalGen-new/blob/main/LICENSE.md */
+/* http://github.com/LocalGen-dev/LocalGen-new/blob/main/LICENSE.md      */
 
 #ifndef __LGCONS_HPP__
 #define __LGCONS_HPP__ 

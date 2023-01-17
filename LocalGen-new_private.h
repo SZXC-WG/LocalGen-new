c2f06af9d67a8b715aec812d02dfed21a9ad663c
chrhaa <101918497+chrhaa@users.noreply.github.com>
2023-01-17 20:16:57 +0800

    。

--- LocalGen-new_private.h
+++ LocalGen-new_private.h
@@ -5,19 +5,19 @@
 #define LOCALGEN-NEW_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.0.0.29"
+#define VER_STRING	"1.0.0.30"
 #define VER_MAJOR	1
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	29
+#define VER_BUILD	30
 #define COMPANY_NAME	"LocalGen-dev"
-#define FILE_VERSION	"1.0.0.29"
+#define FILE_VERSION	"1.0.0.30"
 #define FILE_DESCRIPTION	"Localized Generals.io (new ver.)"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	"Copyright (c) 2023 LocalGen-dev"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"Localized Generals.io (new ver.)"
-#define PRODUCT_VERSION	"1.0.0.29"
+#define PRODUCT_VERSION	"1.0.0.30"
 
 #endif /*LOCALGEN-NEW_PRIVATE_H*/

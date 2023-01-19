db3ae7ebf4d2f3bc0db7a7d5a9041096986d6972
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2023-01-19 15:00:19 +0800

    import xrzBot to LGgame.hpp

--- LGgame.hpp
+++ LGgame.hpp
@@ -18,6 +18,7 @@
 #include <functional>
 #include <algorithm>
 #include <deque>
+#include <vector>
 #include <string>
 #include <chrono>
 #include <random>
@@ -30,7 +31,6 @@ using namespace std::literals;
 // project headers
 #include "LGcons.hpp"
 #include "LGmaps.hpp"
-#include "xrzBot.hpp"
 
 const int dx[5] = {0,-1,0,1,0};
 const int dy[5] = {0,0,-1,0,1};
@@ -98,6 +98,91 @@ int ktqBot(int id,playerCoord coo){
 		if(p[i].del<gameMap[coo.x][coo.y].army) return p[i].to;
 	}
 	return -1;
+}
+int xrzBot(int ind, playerCoord player)
+{
+	static int armyNow;
+	static playerCoord previousPos[16];
+	static int visitTime[305][305][16];
+	static int id;
+	static int turnind[16];
+    static std::mt19937 mtrd(std::chrono::system_clock::now().time_since_epoch().count());
+    armyNow = gameMap[player.x][player.y].army;
+    turnind[ind]++;
+    id = ind;
+    visitTime[player.x][player.y][id]++;
+    if (gameMap[player.x][player.y].army == std::max(0,turnind[id]-100) || gameMap[player.x][player.y].team != id)
+        return 0;
+    struct node
+    {
+        int x, y;
+        int type;
+        int Army;
+        int teamOnIt;
+        int direction;
+    };
+    std::vector<node> operat;
+    for (int i = 1; i <= 4; i++)
+    {
+        node des;
+        des.direction = i;
+        des.x = player.x + dx[i];
+        des.y = player.y + dy[i];
+        if (gameMap[des.x][des.y].type == 2)
+            continue;
+        if (des.x < 1 || des.x > mapH || des.y < 1 || des.y > mapW)
+            continue;
+        des.type = gameMap[des.x][des.y].type;
+        des.Army = gameMap[des.x][des.y].army;
+        des.teamOnIt = gameMap[des.x][des.y].team;
+        if (gameMap[des.x][des.y].team != id && gameMap[des.x][des.y].type == 3)
+        {
+            previousPos[id] = player;
+            return i;
+        }
+        if (des.type == 4 && des.Army <= gameMap[player.x][player.y].army && des.teamOnIt != id)
+        {
+            previousPos[id] = player;
+            return i;
+        }
+    }
+    for (int i = 1; i <= 4; i++)
+    {
+        node des;
+        des.direction = i;
+        des.x = player.x + dx[i];
+        des.y = player.y + dy[i];
+        if (gameMap[des.x][des.y].type == 2)
+            continue;
+        if (des.x < 1 || des.x > mapH || des.y < 1 || des.y > mapW)
+            continue;
+        des.type = gameMap[des.x][des.y].type;
+        des.Army = gameMap[des.x][des.y].army;
+        des.teamOnIt = gameMap[des.x][des.y].team;
+        if (gameMap[des.x][des.y].team != id && gameMap[des.x][des.y].type == 3)
+            return i;
+        if (des.type == 4 && des.Army <= gameMap[player.x][player.y].army)
+            return i;
+        int cnt = 4;
+        if (des.x == previousPos[id].x && des.y == previousPos[id].y)
+            cnt += 2;
+        if (des.teamOnIt != id && des.teamOnIt != 0)
+            cnt--;
+        if (des.type == 0)
+            cnt--;
+        if (des.type == 1)
+            cnt++;
+        if (des.teamOnIt == 0)
+            cnt--;
+        cnt += std::max(0, (int)log2(visitTime[des.x][des.y][id]));
+        if (mtrd() % cnt == 0)
+        {
+            previousPos[id] = player;
+            return i;
+        }
+    }
+    previousPos[id] = player;
+    return mtrd() % 4;
 }
 
 struct gameStatus {
@@ -381,7 +466,7 @@ struct gameStatus {
 					if(!isAlive[i]) continue;
 					switch(robotId[i]) {
 						case 1 ... 50: analyzeMove(i,smartRandomBot(i,coordinate[i]),coordinate[i]); break;
-						case 51 ... 100: analyzeMove(i,xrzBotNamespace::xrzBot(i,coordinate[i]),coordinate[i]); break;
+						case 51 ... 100: analyzeMove(i,xrzBot(i,coordinate[i]),coordinate[i]); break;
 						default: analyzeMove(i,0,coordinate[i]);
 					}
 				}

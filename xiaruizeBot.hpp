99ada7601d2ab11d662bf815837e82bd5e5fabe2
xiaruize0911 <2188298460@qq.com>
2023-01-20 09:46:02 +0800

    Update xiaruizeBot.hpp

--- xiaruizeBot.hpp
+++ xiaruizeBot.hpp
@@ -17,6 +17,7 @@ namespace xiaruizeBot{
     static std::mt19937 mtrd(std::chrono::system_clock::now().time_since_epoch().count());
     int sendArmyProcess[20];
     int backCountCnt[20];
+    playerCoord previousPos[20];
 
     int changeDirection(int x,int &res)
     {
@@ -39,7 +40,7 @@ namespace xiaruizeBot{
 
     int dfs(int id,playerCoord coord)
     {
-        random_shuffle(checkOrder + 1, checkOrder + 4);
+        random_shuffle(checkOrder + 1, checkOrder + 5);
         int x;
         for (int i = 1; i <= 4;i++)
         {
@@ -53,9 +54,11 @@ namespace xiaruizeBot{
             if(gameMap[ToX][ToY].type==3&&gameMap[ToX][ToY].team!=id)
             {
                 operation[id].push_back(x);
+                previousPos[id] = coord;
                 return x;
             }
         }
+        random_shuffle(checkOrder + 1, checkOrder + 5);
         for (int i = 1; i <= 4; i++)
         {
             x = checkOrder[i];
@@ -68,9 +71,11 @@ namespace xiaruizeBot{
             if (gameMap[ToX][ToY].type == 4 && gameMap[ToX][ToY].army<=gameMap[coord.x][coord.y].army&&gameMap[ToX][ToY].team!=id)
             {
                 operation[id].push_back(x);
+                previousPos[id] = coord;
                 return x;
             }
         }
+        random_shuffle(checkOrder + 1, checkOrder + 5);
         for (int i = 1; i <= 4; i++)
         {
             x = checkOrder[i];
@@ -83,18 +88,59 @@ namespace xiaruizeBot{
             if (vis[id][ToX][ToY])
                 continue;
             operation[id].push_back(x);
+            previousPos[id] = coord;
             return x;
         }
-        return -1;
+        random_shuffle(checkOrder + 1, checkOrder + 5);
+        for (int i = 1; i <= 4; i++)
+        {
+            x = checkOrder[i];
+            int ToX = coord.x + dx[x];
+            int ToY = coord.y + dy[x];
+            if (ToX < 1 || ToX > mapH || ToY < 1 || ToY > mapW)
+                continue;
+            if (gameMap[ToX][ToY].type == 2)
+                continue;
+            if(previousPos[id].x==ToX&&previousPos[id].y==ToY)
+                continue;
+            operation[id].push_back(x);
+            previousPos[id] = coord;
+            return x;
+        }
+        random_shuffle(checkOrder + 1, checkOrder + 5);
+        for (int i = 1; i <= 4; i++)
+        {
+            x = checkOrder[i];
+            int ToX = coord.x + dx[x];
+            int ToY = coord.y + dy[x];
+            if (ToX < 1 || ToX > mapH || ToY < 1 || ToY > mapW)
+                continue;
+            if (gameMap[ToX][ToY].type == 2)
+                continue;
+            operation[id].push_back(x);
+            previousPos[id] = coord;
+            return x;
+        }
     }
 
     int xiaruizeBot(int id,playerCoord coord)
     {
         if(gameMap[coord.x][coord.y].army==0||gameMap[coord.x][coord.y].team!=id)
         {
+            memset(vis[id], 0, sizeof(vis[id]));
             backCountCnt[id] = 1;
             sendArmyProcess[id] = 1;
             return 0;
+        }
+        if(sendArmyProcess[id])
+        {
+            if (sendArmyProcess[id] > operation[id].size())
+            {
+                sendArmyProcess[id] = 0;
+                return -1;
+            }
+            sendArmyProcess[id]++;
+            return operation[id][sendArmyProcess[id] - 2];
         }
         vis[id][coord.x][coord.y] = true;
         int returnValue = dfs(id, coord);
@@ -103,28 +149,6 @@ namespace xiaruizeBot{
             backCountCnt[id] = 1;
             sendArmyProcess[id] = 0;
             return returnValue;
-        }
-        if(sendArmyProcess[id])
-        {
-            if(operation[id].size()>=sendArmyProcess[id])
-            {
-                backCountCnt[id] = 1;
-                int res = operation[id][sendArmyProcess[id] - 1];
-                sendArmyProcess[id]++;
-                return res;
-            }
-            else
-            {
-                sendArmyProcess[id] = 0;
-            }
-        }
-        else
-        {
-            int res;
-            changeDirection(operation[id][operation[id].size() -1], res);
-            operation[id].pop_back();
-            backCountCnt[id]++;
-            return res;
         }
     }
 }

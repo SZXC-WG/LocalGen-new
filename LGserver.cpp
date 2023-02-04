1971e785123a3403671e4c672628dcbed329a88e
chrhaa <101918497+chrhaa@users.noreply.github.com>
2023-02-04 17:18:57 +0800

    。

--- LGserver.cpp
+++ LGserver.cpp
@@ -3,13 +3,52 @@
 #include "LGgame.hpp"
 #include "LGzipmap.hpp"
 
-std::mutex mLock;
-int ky,totSock,opt;
-char sendBuf[SSL],recvBuf[SSL];
-SOCKET serverSocket[SSN];
-bool sockCon[SSN],lisEnd;
+//Value
+int opt;
 int stDel,plCnt,rbCnt;
 std::deque<int> movement;
+playerCoord coordinate[64];
+gameStatus game(0,0,0,0);
+
+//Socket
+std::mutex mLock;
+int totSock;
+bool sockCon[SSN],lisEnd;
+SOCKET serverSocket[SSN];
+char sendBuf[SSL],recvBuf[SSL];
+
+//Functions
+void zipSendBuf(){
+	register int p=0,i,j;
+	long long k1=mapH,k2=mapW;
+	sendBuf[p++]=44;
+	sendBuf[p++]=game.playerCnt;
+	
+	for(i=1;i<=game.playerCnt;i++){
+		sendBuf[p++]=coordinate[i].x;
+		sendBuf[p++]=coordinate[i].y;
+	}
+	
+	sendBuf[p++]=PMod(k1)+CHAR_AD;
+	sendBuf[p++]=PMod(k1)+CHAR_AD;
+	sendBuf[p++]=PMod(k2)+CHAR_AD;
+	sendBuf[p++]=PMod(k2)+CHAR_AD;
+	
+	for(i=1;i<=mapH;i++)
+	for(j=1;j<=mapW;j++){
+		sendBuf[p++]=gameMap[i][j].team+CHAR_AD;
+		sendBuf[p]=(gameMap[i][j].type<<2)+(gameMap[i][j].lit<<1);
+		k1=gameMap[i][j].army;
+		
+		if(k1<0){
+			k1=-k1;
+			sendBuf[p++]+=CHAR_AD+1;
+		}else sendBuf[p++]+=CHAR_AD;
+		
+		for(k2=1;k2<=8;k2++)
+		sendBuf[p++]=PMod(k1)+CHAR_AD;
+	}sendBuf[p]='\0';
+}
 
 void sockListen(){
 	if(initSock())
@@ -61,14 +100,27 @@ void sockListen(){
 	return ;
 }
 
-void procMessage(){
-	if()
+void procMessage(int sockID){
+	if(recvBuf[0]==43){
+		if(recvBuf[1]==CHAR_AD)
+		sockCon[sockID]=false;
+		else{
+			if(lisEnd) sendBuf[1]=CHAR_AD;
+			else sendBuf[1]=sockID+CHAR_AD;
+			
+			sendBuf[0]=43;
+			sendBuf[2]='\0';
+			send(serverSocket[sockID],sendBuf,sizeof(sendBuf),0);
+		}
+	}else{
+		
+	}
 }
 
 void sockBroadcast(){
 	std::lock_guard<std::mutex> mGuard(mLock);
 	
-	for(int i=1;i<totSock;i++)
+	for(int i=1;i<totSock;i++) if(sockCon[i])
 	send(serverSocket[i],sendBuf,sizeof(sendBuf),0);
 	
 	memset(sendBuf,0,sizeof(sendBuf));
@@ -77,113 +129,110 @@ void sockBroadcast(){
 void sockCollect(){
 	std::lock_guard<std::mutex> mGuard(mLock);
 	
-	for(int i=1;i<totSock;i++){
+	for(int i=1;i<totSock;i++) if(sockCon[i]){
 		int res=recv(serverSocket[i],recvBuf,sizeof(recvBuf),0);
-		if(res>0) procMessage();
+		if(res>0) procMessage(i);
 		memset(recvBuf,0,sizeof(recvBuf));
 	}
 }
 
 int serverGame(){
-	gameStatus GAME(1,1048575,plCnt+rbCnt,stDel);
+	game.isWeb=1;
+	game.cheatCode=1048575;
+	game.playerCnt=plCnt+rbCnt;
+	game.stepDelay=stDel;
 	
-	if (GAME.played)
+	if (game.played)
 		return -1;
 		
-	GAME.played = 1;
-	GAME.gameMesC = 0;
+	game.played = 1;
+	game.gameMesC = 0;
 	
-	if (!GAME.isWeb)
+	if (!game.isWeb)
 	{
 		int robotId[64];
-		playerCoord coordinate[64];
 		std::mt19937 mtrd(std::chrono::system_clock::now().time_since_epoch().count());
 		
 		for (int i = plCnt+1; i <= plCnt+rbCnt; ++i)
 			robotId[i] = mtrd() % 300 + 1;
 			
-		GAME.initGenerals(coordinate);
-		GAME.updateMap();
-		GAME.curTurn = 0;
+		game.initGenerals(coordinate);
+		game.updateMap();
+		game.curTurn = 0;
 		bool gameEnd = 0;
 		
-		for (; is_run(); delay_fps(GAME.stepDelay))
+		for (; is_run(); delay_fps(game.stepDelay))
 		{
-			GAME.updateMap();
+			game.updateMap();
 			sockCollect();
 			
-			while (!movement.empty() && GAME.analyzeMove(1, movement.front(), coordinate[1]))
+			while (!movement.empty() && game.analyzeMove(1, movement.front(), coordinate[1]))
 				movement.pop_front();
 			if (!movement.empty())
 				movement.pop_front();
 			
 			for (int i = plCnt+1; i <= plCnt+rbCnt; ++i)
 			{
-				if (!GAME.isAlive[i])
+				if (!game.isAlive[i])
 					continue;
 				switch (robotId[i])
 				{
 				case 1 ... 100:
-					GAME.analyzeMove(i, smartRandomBot::smartRandomBot(i, coordinate[i]), coordinate[i]);
+					game.analyzeMove(i, smartRandomBot::smartRandomBot(i, coordinate[i]), coordinate[i]);
 					break;
 				case 101 ... 200:
-					GAME.analyzeMove(i, xrzBot::xrzBot(i, coordinate[i]), coordinate[i]);
+					game.analyzeMove(i, xrzBot::xrzBot(i, coordinate[i]), coordinate[i]);
 					break;
 				case 201 ... 300:
-					GAME.analyzeMove(i, xiaruizeBot::xiaruizeBot(i, coordinate[i]), coordinate[i]);
+					game.analyzeMove(i, xiaruizeBot::xiaruizeBot(i, coordinate[i]), coordinate[i]);
 					break;
 				default:
-					GAME.analyzeMove(i, 0, coordinate[i]);
+					game.analyzeMove(i, 0, coordinate[i]);
 				}
 			}
 			
-			GAME.flushMove();
-			zipStatus();
-			int k=0;
-			
-			for(k=0;strZipStatus[k]!='\0';k++)
-			sendBuf[k]=strZipStatus[k];
-			
-			sendBuf[k]='\0';
+			game.flushMove();
+			zipSendBuf();
 			sockBroadcast();
 			
-			if (GAME.cheatCode != 1048575)
+			if (game.cheatCode != 1048575)
 			{
 				int alldead = 0;
-				for (int i = 1; i <= GAME.playerCnt && !alldead; ++i)
+				for (int i = 1; i <= game.playerCnt && !alldead; ++i)
 				{
-					if (GAME.cheatCode & (1 << i))
-						if (GAME.isAlive[i])
+					if (game.cheatCode & (1 << i))
+						if (game.isAlive[i])
 							alldead = 1;
 				}
 				if (!alldead)
 				{
-					GAME.cheatCode = 1048575;
+					game.cheatCode = 1048575;
 					MessageBox(nullptr, "ALL THE PLAYERS YOU SELECTED TO BE SEEN IS DEAD.\nTHE OVERALL CHEAT MODE WILL BE SWITCHED ON.", "TIP", MB_OK | MB_SYSTEMMODAL);
 				}
 			}
 			if (!gameEnd)
 			{
 				int ed = 0;
-				for (int i = 1; i <= GAME.playerCnt; ++i)
-					ed |= (GAME.isAlive[i] << i);
+				for (int i = 1; i <= game.playerCnt; ++i)
+					ed |= (game.isAlive[i] << i);
 				if (__builtin_popcount(ed) == 1)
 				{
 					MessageBox(nullptr,
 							   ("PLAYER " + defTeams[std::__lg(ed)].name + " WON!" + "\n" +
-								"THE GAME WILL CONTINUE." + "\n" +
+								"THE game WILL CONTINUE." + "\n" +
 								"YOU CAN PRESS [ESC] TO EXIT.")
 								   .c_str(),
-							   "GAME END", MB_OK | MB_SYSTEMMODAL);
+							   "game END", MB_OK | MB_SYSTEMMODAL);
 					gameEnd = 1;
-					GAME.winnerNum = std::__lg(ed);
-					GAME.cheatCode = 1048575;
+					game.winnerNum = std::__lg(ed);
+					game.cheatCode = 1048575;
 				}
 			}
 		}
 	}
 }
 
+//Main
 signed main(){
 	initattr();
 	puts("This is the server of LocalGenerals.");

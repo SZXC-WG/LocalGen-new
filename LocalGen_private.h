1c70e69a4cc96ae6028701a17cf73d2ea09ad540
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2023-03-05 22:44:35 +0800

    dev update

--- LocalGen_private.h
+++ LocalGen_private.h
@@ -5,19 +5,19 @@
 #define LOCALGEN_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"3.0.0.1221"
+#define VER_STRING	"3.0.0.1241"
 #define VER_MAJOR	3
 #define VER_MINOR	0
 #define VER_RELEASE	0
-#define VER_BUILD	1221
+#define VER_BUILD	1241
 #define COMPANY_NAME	"LocalGen-dev"
-#define FILE_VERSION	"3.0.0.1221"
+#define FILE_VERSION	"3.0.0.1241"
 #define FILE_DESCRIPTION	"Local Generals.io (new ver.)"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	"Copyright (c) 2023 LocalGen-dev"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"Local Generals.io (new ver.)"
-#define PRODUCT_VERSION	"3.0.0.1221"
+#define PRODUCT_VERSION	"3.0.0.1241"
 
 #endif /*LOCALGEN_PRIVATE_H*/

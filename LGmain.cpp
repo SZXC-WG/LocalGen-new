77d9c4013937dd4afa674089117581582232ba5f
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2024-08-22 09:19:37 +0800

    raise in-code cxx standard to ISO

--- LGmain.cpp
+++ LGmain.cpp
@@ -14,7 +14,7 @@
 #ifndef __LGMAIN_CPP__
 #define __LGMAIN_CPP__
 
-#if __cplusplus < 201300L
+#if __cplusplus < 201402L
 #	error This program should be compiled under the C++14\
 standard or higher. If you use the C++ standard under\
 it, please use a standard greater. In G++, you can\

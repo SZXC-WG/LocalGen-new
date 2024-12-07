9b5d1b7e8bb7a85e279ad5dfbf13f4b0dac7f574
Stanley <stanley_shao@126.com>
2024-12-07 14:22:36 +0800

    refactor directories

---
+++ src/mainWindow/mainWindow.cpp
@@ -0,0 +1,11 @@
+#include "mainWindow.h"
+#include "./ui_mainWindow.h"
+
+MainWindow::MainWindow(QWidget* parent) :
+    QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}

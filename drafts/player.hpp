37f8cc89fbe6da245e4360de3999557b8e9d894c
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2025-01-18 13:00:36 +0800

    add player index

--- drafts/player.hpp
+++ drafts/player.hpp
@@ -11,12 +11,18 @@
 #ifndef LGEN_MODULE_GE_PLAYER
 #define LGEN_MODULE_GE_PLAYER 1
 
+#include <cstdint>
 #include <string>
 
 /// Base struct for players.
 class Player {
    public:
+    using index_t = std::uint32_t;
+
+   public:
     const std::string name;
+    index_t index;
+    
     Player(const std::string& name) :
         name(name) {}
     virtual ~Player() {}

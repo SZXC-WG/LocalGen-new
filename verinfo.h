4ac953e3fc03c2a61640900d9a0ac7ea1f05afa5
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2025-05-11 22:25:56 +0800

    update version number to v4.3.0

--- verinfo.h
+++ verinfo.h
@@ -1,5 +1,5 @@
-#define VER_STRING "4.2.6.2956"
+#define VER_STRING "4.3.0.2957"
 #define VER_MAJOR   4
-#define VER_MINOR   2
-#define VER_RELEASE	6
-#define VER_BUILD   2956
+#define VER_MINOR   3
+#define VER_RELEASE	0
+#define VER_BUILD   2957

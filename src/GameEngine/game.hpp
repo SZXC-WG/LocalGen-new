8c348469f921ce4bfbb0af50839402b42e64c4ce
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2025-06-27 23:24:09 +0800

    feat: add InGameBoard, moveProcessor amd enum game_message_e

--- src/GameEngine/game.hpp
+++ src/GameEngine/game.hpp
@@ -14,6 +14,119 @@
 #include "board.hpp"
 #include "player.hpp"
 
-class BasicGame {};
+enum game_message_e {
+    GAME_MESSAGE_WIN,
+    GAME_MESSAGE_CAPTURE,
+    GAME_MESSAGE_SURRENDER,
+};
+
+class BasicGame {
+   public:
+    // type aliases
+    using turn_t = uint32_t;
+
+   public:
+    turn_t curTurn;
+
+   public:
+    /// Broadcast a game message to the game.
+    /// @param message type of the message.
+    /// @param associatedList the needed extra information of the message.
+    void broadcast(game_message_e message,
+                   std::initializer_list<Player*> associatedList) {}
+
+    class InGameBoard : public Board {
+       public:
+        BasicGame* game;
+
+       public:
+        /// The default constructor is deleted, for an %InGameBoard relies on a
+        /// specific game object.
+        InGameBoard() = delete;
+        InGameBoard(BasicGame* _game) : game(_game) {}
+
+       public:
+        class MoveProcessor;
+        friend class InGameBoard::MoveProcessor;
+        /// Move Processor used by games.
+        /// A %MoveProcessor is used to contain and process moves.
+        class MoveProcessor {
+           protected:
+            std::deque<Move> movesInQueue;
+
+           protected:
+            BasicGame* game;
+            InGameBoard* board;
+
+            /// Constructors.
+            /// The default constructor is deleted, for a %MoveProcessor must be
+            /// linked to a %Board.
+           public:
+            MoveProcessor() = delete;
+            MoveProcessor(BasicGame* _game, InGameBoard* _board)
+                : game(_game), board(_board) {}
+
+           public:
+            /// Add a %Move to the waiting-to-be-processed queue.
+            /// @param move added %Move.
+            inline void add(Move move) {
+                if (move.available(board)) movesInQueue.emplace_back(move);
+            }
+
+            /// Sort the in-queue moves according to their priority.
+            /// May not be implemented.
+            void sort() {}
+
+            /// Perform an action where a player captures another player.
+            /// @param p1 the player that captures.
+            /// @param p2 the player that is captured.
+            void capture(Player* p1, Player* p2) {
+                for (auto row : board->tiles) {
+                    for (auto tile : row) {
+                        if (tile.occupier == p2) {
+                            tile.occupier = p1;
+                            if (tile.type == TILE_GENERAL)
+                                tile.type = TILE_CITY;
+                        }
+                    }
+                }
+                game->broadcast(GAME_MESSAGE_CAPTURE, {p1, p2});
+            }
+            /// Execute all the in-queue moves, one by one.
+            void execute() {
+                for (auto move : movesInQueue) {
+                    if (!move.available(board)) continue;
+                    Tile& fromTile = board->getTile(move.from);
+                    Tile& toTile = board->getTile(move.to);
+                    army_t takenArmy = fromTile.army;
+                    if (move.takeHalf) takenArmy >>= 1;
+                    fromTile.army -= takenArmy;
+                    toTile.army -= takenArmy;
+                    if (toTile.army < 0) {
+                        toTile.army = -toTile.army;
+                        if (toTile.type == TILE_GENERAL)
+                            capture(move.player, toTile.occupier);
+                        toTile.occupier = move.player;
+                    }
+                }
+            }
+        };
+
+       public:
+        MoveProcessor processor{game, this};
+    };
+
+   protected:
+    InGameBoard board{this};
+
+   public:
+    inline void act(Player* player) {
+        while (!player->moveQueue.empty() &&
+               !player->moveQueue.front().available(&board))
+            player->moveQueue.pop_front();
+        if (player->moveQueue.empty()) return;
+        board.processor.add(player->moveQueue.front());
+    }
+};
 
 #endif  // LGEN_MODULE_GE_GAME

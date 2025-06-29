813e52f7e4fb62daff5f1f61f3a20c2314340355
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2025-06-29 19:47:03 +0800

    chore: update version number

--- verinfo.h
+++ verinfo.h
@@ -1,5 +1,5 @@
-#define VER_STRING "4.3.1.3066"
+#define VER_STRING "4.3.2.3068"
 #define VER_MAJOR   4
 #define VER_MINOR   3
-#define VER_RELEASE	1
-#define VER_BUILD   3066
+#define VER_RELEASE	2
+#define VER_BUILD   3068

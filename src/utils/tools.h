0eec98a3081b683c2f633fa2d407d2bdd4b01987
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2025-07-01 20:47:03 +0800

    upd: remove overall random

--- src/utils/tools.h
+++ src/utils/tools.h
@@ -9,11 +9,4 @@
 #ifndef LGEN_LIB_TOOLS
 #define LGEN_LIB_TOOLS 1
 
-#include <random>
-
-/// default random-integer generators
-
-std::mt19937 random{std::random_device()()};
-std::mt19937_64 random64{std::random_device()()};
-
 #endif

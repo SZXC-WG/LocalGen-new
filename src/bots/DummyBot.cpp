2f706f70f905acdb6564c9bd33c432d7b9d192a4
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2025-08-16 23:40:01 +0800

    add: DummyBot (unfinished) edition 0.1

---
+++ src/bots/DummyBot.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file dummyBot.cpp
+ *
+ * PLEASE READ THIS COMMENT AND THIS FILE CAREFULLY BEFORE YOU START TO
+ * IMPLEMENT YOUR OWN BOT.
+ *
+ * This "DummyBot" is a simple example of how to create a bot.
+ * Developers can refer to this file when implementing their own bots.
+ * The bot implemented a pretty basic algorithm, just being an example.
+ *
+ * Like this file, bot codes should be put into the `src/bots` folder.
+ *
+ * @copyright Copyright (c) SZXC Work Group.
+ */
+
+#ifndef LGEN_BOTS_DUMMYBOT
+#define LGEN_BOTS_DUMMYBOT
+
+// Remember to include the bot header file.
+// Without this, you can't define your bot class, and the registry won't work.
+#include "../GameEngine/bot.h"
+
+// DO NOT PUT YOUR BOT CODE IN A NAMESPACE. THAT WILL MAKE THE REGISTRY NOT
+// WORK. AND SO, CHOOSE YOUR BOT NAME CAREFULLY, IT SHOULD BE UNIQUE.
+
+class DummyBot : public BasicBot {
+   protected:
+    explicit DummyBot() : BasicBot("DummyBot") {}
+
+   protected:
+    // You can put whatever you want, whatever you need here.
+    // Don't define non-const static variables, they may cause thread problems.
+
+   public:
+    void compute() override {}
+};
+
+REGISTER_BOT(DummyBot, "DummyBot")
+
+#endif  // LGEN_BOTS_DUMMYBOT

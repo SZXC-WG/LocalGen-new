80947ecb796f338e5b400fb48f252d1fe8836465
Stanley <stanley_shao@126.com>
2026-02-14 13:09:52 +0800

    upd(localGameWindow): helper functions

--- src/localGameWindow/localGameWindow.cpp
+++ src/localGameWindow/localGameWindow.cpp
@@ -1,5 +1,6 @@
 #include "localGameWindow.h"
 
+#include <QRandomGenerator>
 #include <QTimer>
 #include <QVBoxLayout>
 
@@ -12,6 +13,72 @@ void HumanPlayer::requestMove(BoardView& boardView) {
     // TODO: send boardView to UI
 }
 
+namespace {
+
+inline QColor playerColor(index_t playerId) {
+    static const QColor palette[16] = {
+        "#FF0000", "#2792FF", "#008000", "#008080", "#FF7010", "#F032E6",
+        "#800080", "#9B0101", "#B3AC32", "#9A5E24", "#1031FF", "#594CA5",
+        "#85A91C", "#F87375", "#B47FCA", "#B49971"};
+    return palette[playerId % 16];
+}
+
+inline DisplayTile toDisplayTile(const TileView& tile) {
+    DisplayTile display;
+    display.type = tile.type;
+    display.lightIcon = false;
+    if (!tile.visible) {
+        display.color.setRgb(57, 57, 57);
+        display.text.clear();
+        return display;
+    }
+    if (tile.occupier >= 0) {
+        display.color = playerColor(tile.occupier);
+    } else {
+        switch (tile.type) {
+            case TILE_MOUNTAIN:
+            case TILE_LOOKOUT:
+            case TILE_OBSERVATORY:
+            case TILE_OBSTACLE:    display.color.setRgb(187, 187, 187); break;
+            case TILE_SWAMP:
+            case TILE_CITY:        display.color.setRgb(128, 128, 128); break;
+            default:               display.color.setRgb(220, 220, 220); break;
+        }
+    }
+    if (tile.army != 0 || tile.type == TILE_CITY) {
+        display.text = QString::number(tile.army);
+        if (tile.occupier == -1 && tile.type == TILE_NEUTRAL) {
+            display.color.setRgb(128, 128, 128);
+        }
+    } else {
+        display.text.clear();
+    }
+    return display;
+}
+
+InitBoard createRandomBoard(int width, int height) {
+    InitBoard board(height, width);
+
+    QRandomGenerator* rng = QRandomGenerator::global();
+    int area = width * height;
+    int numMountains = rng->bounded(area / 8, area / 8 + area / 20 + 1);
+    for (int i = 0; i < numMountains; ++i) {
+        Coord pos(rng->bounded(height) + 1, rng->bounded(width) + 1);
+        board.changeTile(pos, Tile(-1, TILE_MOUNTAIN, 0));
+    }
+
+    int numCities = rng->bounded(area / 30, area / 15 + 1);
+    for (int i = 0; i < numCities; ++i) {
+        Coord pos(rng->bounded(height) + 1, rng->bounded(width) + 1);
+        army_t army = static_cast<army_t>(rng->bounded(40, 50));
+        board.changeTile(pos, Tile(-1, TILE_CITY, army));
+    }
+
+    return board;
+}
+
+}  // namespace
+
 LocalGameWindow::LocalGameWindow(QWidget* parent, const LocalGameConfig& config)
     : QDialog(parent) {
     setWindowTitle("Local Game");
@@ -21,9 +88,11 @@ LocalGameWindow::LocalGameWindow(QWidget* parent, const LocalGameConfig& config)
 
     gameMap = new MapWidget(this, config.mapWidth, config.mapHeight);
     // testing configuration
-    for (int r = 10; r < config.mapHeight; r++) {
-        for (int c = 10; c < config.mapWidth; c++) {
-            gameMap->tileAt(r, c).color = QColor(57, 57, 57);
+    BoardView view =
+        createRandomBoard(config.mapWidth, config.mapHeight).view(0);
+    for (int r = 0; r < config.mapHeight; r++) {
+        for (int c = 0; c < config.mapWidth; c++) {
+            gameMap->tileAt(r, c) = toDisplayTile(view.tileAt(r + 1, c + 1));
         }
     }
     QVBoxLayout* layout = new QVBoxLayout(this);

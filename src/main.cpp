8b2ac08ff9ff9aed336bee6911de80f669f3379d
Stanley <stanley_shao@126.com>
2026-03-06 21:45:44 +0800

    fix: UI changing with system color theme

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
+#include <QStyleHints>
 
 #include "ui/mainWindow/mainWindow.h"
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     app.setStyle("windows11");
+    QApplication::styleHints()->setColorScheme(Qt::ColorScheme::Light);
     MainWindow window;
     window.show();
     return app.exec();

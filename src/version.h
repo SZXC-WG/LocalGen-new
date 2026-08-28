7ccd07e772605db0d8fbdbcab572cee252cee718
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2026-08-28 11:44:46 +0800

    feat: new version control header

---
+++ src/version.h
@@ -0,0 +1,54 @@
+// Copyright (C) 2026 SZXC Work Group
+// SPDX-License-Identifier: MIT
+
+/**
+ * version.h
+ *
+ * New version control of LG v5
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace version_namespace {
+
+enum class BuildType { Rel, RC, Beta, Dev };
+
+typedef std::uint16_t count_t;
+
+inline std::string buildStr(BuildType type = BuildType::Dev,
+                            count_t count = 0) {
+    switch (type) {
+        case BuildType::Rel:  return "";
+        case BuildType::RC:   return "-rc." + std::to_string(count);
+        case BuildType::Beta: return "-beta." + std::to_string(count);
+        case BuildType::Dev:  return "-dev";
+    }
+    return "-unknown";
+}
+
+struct Version {
+    count_t major, minor, patch;
+    std::string suffix;
+
+    Version() = default;
+    constexpr Version(count_t major, count_t minor, count_t patch,
+                      BuildType type = BuildType::Dev, count_t count = 0)
+        : major(major),
+          minor(minor),
+          patch(patch),
+          suffix(buildStr(type, count)) {}
+
+    std::string str() const {
+        return std::to_string(major) + "." + std::to_string(minor) + "." +
+               std::to_string(patch) + suffix;
+    }
+};
+
+const Version version{4, 3, 4, BuildType::Dev};
+
+}  // namespace version_namespace
+
+using version_namespace::version;

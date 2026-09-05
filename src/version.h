4f33f10a1cbe980b3e3397811aa9b1bf10b7abfb
AppOfficer <73411359+AppOfficer@users.noreply.github.com>
2026-09-05 14:26:48 +0800

    refactor(version): make `version` constexpr

--- src/version.h
+++ src/version.h
@@ -18,6 +18,12 @@ enum class BuildType : std::uint8_t { Rel, RC, Beta, Alpha, Dev };
 
 typedef std::uint16_t count_t;
 
+#define VER_MAJOR 4
+#define VER_MINOR 3
+#define VER_PATCH 4
+#define VER_TYPE BuildType::Dev
+#define VER_COUNT
+
 inline std::string buildStr(BuildType type = BuildType::Dev,
                             count_t count = 0) {
     switch (type) {
@@ -32,24 +38,24 @@ inline std::string buildStr(BuildType type = BuildType::Dev,
 
 struct Version {
     count_t major, minor, patch;
-    std::string suffix;
+    BuildType type;
+    count_t count;
 
     Version() = default;
     constexpr Version(count_t major, count_t minor, count_t patch,
                       BuildType type = BuildType::Dev, count_t count = 0)
-        : major(major),
-          minor(minor),
-          patch(patch),
-          suffix(buildStr(type, count)) {}
+        : major(major), minor(minor), patch(patch), type(type), count(count) {}
 
     std::string str() const {
         return std::to_string(major) + "." + std::to_string(minor) + "." +
-               std::to_string(patch) + suffix;
+               std::to_string(patch) + buildStr(type, count);
     }
 };
 
-const Version version{4, 3, 4, BuildType::Dev};
+constexpr Version version{VER_MAJOR, VER_MINOR, VER_PATCH, VER_TYPE, VER_COUNT};
 
 }  // namespace version_namespace
 
 using version_namespace::version;
+
+#define VER_STRING version.str().c_str()
